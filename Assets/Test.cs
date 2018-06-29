82584587a8129300d7c25cb4418dcd793597d68e
nonochan13 <ueno.jit@gmail.com>
2018-06-29 20:37:32 +0900

    完成したものです。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,61 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public class Boss
+{
+    private int mp = 53;          // 
+    private int power = 30;
+    // 
+    public void Attack()
+    {
+        Debug.Log(this.power + "魔法を攻撃をした。残りMPは〇〇");
+    }
+    // mpが足りない場合の関数
+    public void Magic(int cost)
+    {
+
+        if (mp<5)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+        else
+        {
+            // mpを5減らす
+            this.mp = mp - cost;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+
+    }
+
+}
+
 public class Test : MonoBehaviour
 {
-    //use this for initialization
+
     void Start()
     {
         //要素数5の配列を初期化する
         int[] array = { 2, 5, 7, 1, 6, };
 
         //配列の要素をすべて表示する
-        for (int i=array.Length -1; 0<=i;i--)
+        for (int i = array.Length - 1; 0 <= i; i--)
         {
             Debug.Log(array[i]);
         }
-    }
 
-    // Update is called once per frame
-    void Update() {
-
-        }
-    
-}
-
-
-public class Boss
-{
-    private int mp = 53;          // 
-
-    // 
-    public void Attack()
-    {
-        Debug.Log(this.power + "魔法を攻撃をした。残りMPは〇〇");
-    }
-
-    // mpが足りない場合の関数
-    public void Defence(int damage)
-    {
-        Debug.Log(damage + "MPが足りないため魔法が使えない。");
-    }
-
-}
-
-public class Test : MonoBehaviour
-{
-
-    void Start()
-    {
         // Bossクラスの変数を宣言する
         Boss lastboss = new Boss();
 
         // 魔法攻撃用の関数を呼び出す
         lastboss.Attack();
+
         // 10回処理を繰り返す
-        for (int i = 1; i < 10; i++)
-        // 防御用の関数を呼び出す
-        lastboss.Defence(3);
+        for (int i = 0; i < 11; i++)
+        {
+            // 防御用の関数を呼び出す
+            lastboss.Magic(5);
+        }
     }
 
     // Update is called once per frame
